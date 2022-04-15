03d628c425f49f21a27ef5e54545868f5f589573
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-04-15 12:39:28 -0500

    Update Ejercicio3-Twitter.js

--- weekly_mission_2/exercises/Ejercicio3/Ejercicio3-Twitter.js
+++ weekly_mission_2/exercises/Ejercicio3/Ejercicio3-Twitter.js
@@ -1,5 +1,5 @@
 class TwitterUser{
-    constructor(name,username,bio,birthday,location,userCreate,hashtags,usersFollowed,trending_topic){
+    constructor(name,username,bio,birthday,location,userCreate,following,followers){
         
         this.name=name,
         this.username=username,
@@ -7,26 +7,39 @@ class TwitterUser{
         this.birthday=birthday,
         this.location=location,
         this.userCreate=userCreate,
-        
-        this.hashtags=hashtags,
-        this.usersFollowed=usersFollowed
-        this.trending_topic=trending_topic
+        this.following=following,
+        this.followers=followers
     }
     
     get getName(){
         return `Usuario ${this.username} con nombre ${this.name}`
     }
-
     get getUserData(){
-        return `Cuenta creada el ${this.userCreate}, con localización en ${this.location}`
-    }
-
-    get getTrendings(){
-        return `Al usuario ${this.username} le aparece los siguientes trendings: "${this.trending_topic}"`
+        return `Cuenta creada el ${this.userCreate}, con ${this.followers} followers, sigue a ${this.following} cuentas`
     }
 }
 
-const ReportTwitter= new TwitterUser("Eirikr","@ErickDKnight","meh","4/Septiembre/1995","Michoacán,México","mayo/2013",["#MaxKAiserEnSCMx","#LISA","#TeamLlevatelo","MedallaExa","#Patrias"],["@Charlielynnmx", "@incorrectos_mx","@eldollop","@elECpodcast","@TomKingTK","@pasquinmx"],["EVERLASTING MEDALIST","Slipknot","Kyrie","Shanks","Informe"])
-console.log(ReportTwitter)
-console.log(ReportTwitter.getName)
-console.log(ReportTwitter.getTrendings)
+class TweetData{
+    constructor(Message,url,date,retweets,comments,likes){
+        this.Message=Message,
+        this.url=url,
+        this.date=date,
+        this.retweets=retweets,
+        this.comments=comments,
+        this.likes=likes
+    }
+    get tweetData(){
+        return `Tweet:"${this.Message}" con \n ${this.retweets} retweets, ${this.comments} comentarios y ${this.likes} likes`
+    }
+}
+
+
+
+const ReportUser= new TwitterUser("Eirikr","@ErickDKnight","meh","4/Septiembre/1995","Michoacán,México","mayo/2013",131,66);
+const ReportTweet=new TweetData("20 min más tarde y no alcanzaba vacuna","https://twitter.com/ErickDKnight/status/1428553742580916224","10:05 p. m. · 19 ago. 2021",0,0,0);
+
+console.log(ReportUser)
+console.log(ReportTweet)
+console.log(ReportUser.getName)
+console.log(ReportTweet.tweetData)
+console.log(ReportUser.getUserData)
