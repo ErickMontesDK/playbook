9d09fa4fe1e3439b0763d2c702f9178ec07646ca
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-04-15 12:26:06 -0500

    Update Ejercicio1-Twitter.js

--- weekly_mission_2/exercises/Ejercicio1/Ejercicio1-Twitter.js
+++ weekly_mission_2/exercises/Ejercicio1/Ejercicio1-Twitter.js
@@ -1,4 +1,5 @@
 const TwitterUser={
+    //Perfil del usuario
     user:{
         name:"Eirikr",
         username:"@ErickDKnight",
@@ -6,22 +7,28 @@ const TwitterUser={
         birthday:"4/Septiembre/1995",
         location:"Michoacán,México",
         userCreate:"mayo/2013",
+        following:131,
+        followers:66,
     },
-    hashtags:["#MaxKAiserEnSCMx","#LISA","#TeamLlevatelo","MedallaExa","#Patrias"],
-    usersFollowed:["@Charlielynnmx", "@incorrectos_mx","@eldollop","@elECpodcast","@TomKingTK","@pasquinmx"],
-    trending_topic:["EVERLASTING MEDALIST","Slipknot","Kyrie","Shanks","Informe"],
+    tweet:{
+        Message:"20 min más tarde y no alcanzaba vacuna",
+        url:"https://twitter.com/ErickDKnight/status/1428553742580916224",
+        date:"10:05 p. m. · 19 ago. 2021",
+        retweets:0,
+        comments:0,
+        likes:0,
+    },
 
     get getName(){
         return `Usuario ${this.user.username} con nombre ${this.user.name}`
     },
-
-    get getUserData(){
-        return `Cuenta creada el ${this.user.userCreate}, con localización en ${this.user.location}`
+    get tweetData(){
+        return `Tweet:"${this.tweet.Message}" con \n ${this.tweet.retweets} retweets, ${this.tweet.comments} comentarios y ${this.tweet.likes} likes`
     },
-    get getTrendings(){
-        return `Al usuario ${this.user.username} le aparece los siguientes trendings: "${this.trending_topic}"`
-    }
-}
-console.log(TwitterUser)
-console.log(TwitterUser.getName)
-console.log(TwitterUser.getTrendings)
+    get getUserData(){
+        return `Cuenta creada el ${this.user.userCreate}, con ${this.user.followers} followers, sigue a ${this.user.following} cuentas`
+    },
+} 
+console.log(TwitterUser.tweetData);
+console.log(TwitterUser.getName);
+console.log(TwitterUser.getUserData);
